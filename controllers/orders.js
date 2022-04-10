951097172e4cda140ef3954391e4de315fee44c9
Nicolas Chiarello <nicolasch.fcm@gmail.com>
2022-04-10 12:47:30 -0300

    test

--- controllers/orders.js
+++ controllers/orders.js
@@ -82,10 +82,9 @@ exports.verify = async (req,res) => {
   const id = req.query.id
   try {
     console.log(id);
-    const payment = await axios.get(`https://api.mercadopago.com/v1/payments/${id}`,{Autorization: "Bearer TEST-4118311813066874-032322-792fbd5a35ee362bd406466a107faae5-659536649"});
-    console.log(payment);
 
-    res.status(200).json({id: id, msg: payment})
+
+    res.status(200).json({id: id})
     
   } catch (error) {
     res.status(500).json({error});
