3213caaedbbe1a8768a17cc39506de0c775c84b2
Nicolas Chiarello <nicolasch.fcm@gmail.com>
2022-04-10 11:40:22 -0300

    verifytest

--- routes/orders.js
+++ routes/orders.js
@@ -4,13 +4,7 @@ const orderRoutes = require('../controllers/orders')
 
 router.get("/getall", orderRoutes.getAll)
 router.post("/create", orderRoutes.createOrder)
-router.post("/verify", (req,res)=>{
-    try {
-        res.status(200).json(req,res)
-    } catch (error) {
-        res.status(400).json(error)
-    }
-})
+router.post("/verify", orderRoutes.verify)
 
 
 module.exports = router
