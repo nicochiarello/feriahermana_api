c2e5be9b98ef4ca060bfe1e3d073db223f7fb092
Nicolas Chiarello <nicolasch.fcm@gmail.com>
2022-04-17 13:42:21 -0300

    final fix

--- controllers/users.js
+++ controllers/users.js
@@ -8,32 +8,34 @@ exports.createUser = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     res.status(400).json(errors);
-  }
-  const roles = req.body.roles;
-  try {
-    const user = {
-      name: req.body.name,
-      email: req.body.email,
-    };
-    if (roles) {
-      const foundRoles = await Roles.findOne({ name: { $in: roles } });
-      user.roles = foundRoles._id;
-    } else {
-      const role = await Roles.findOne({ name: "user" });
-      user.roles = [role._id];
+  } else {
+
+    const roles = req.body.roles;
+    try {
+      const user = {
+        name: req.body.name,
+        email: req.body.email,
+      };
+      if (roles) {
+        const foundRoles = await Roles.findOne({ name: { $in: roles } });
+        user.roles = foundRoles._id;
+      } else {
+        const role = await Roles.findOne({ name: "user" });
+        user.roles = [role._id];
+      }
+      const hashedPassword = await bcrypt.hash(req.body.password, 12);
+      user.password = hashedPassword;
+      const create = await new User(user);
+      await create.save();
+      const token = await jwt.sign(
+        { email: create.email, userId: create._id.toString(), userRoles: user.roles },
+        "feriahermanaAPI2425",
+        { expiresIn: "1h" }
+      );
+      res.status(200).json({ msg: "user created", user: create, token, roles:user.roles });
+    } catch (error) {
+      res.status(400).json({ msg: error });
     }
-    const hashedPassword = await bcrypt.hash(req.body.password, 12);
-    user.password = hashedPassword;
-    const create = await new User(user);
-    await create.save();
-    const token = await jwt.sign(
-      { email: create.email, userId: create._id.toString(), userRoles: user.roles },
-      "feriahermanaAPI2425",
-      { expiresIn: "1h" }
-    );
-    res.status(200).json({ msg: "user created", user: create, token, roles:user.roles });
-  } catch (error) {
-    res.status(400).json({ msg: error });
   }
 };
 
