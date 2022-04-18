cc366f91001ac54821b6e631c653f413cd6c961d
Nicolas Chiarello <nicolasch.fcm@gmail.com>
2022-04-18 17:41:33 -0300

    test

--- controllers/orders.js
+++ controllers/orders.js
@@ -163,16 +163,16 @@ exports.deleteSingleOrder = async (req,res) => {
     const deleteProducts = async () => {
       req.body.products.forEach(async (i)=> {
         let product =  await Products.findById(i._id)
-        const params = {
-          Bucket: "feria-hermana",
-          Key: product.img.split(".com/")[1],
-        };
-        console.log(params);
-        s3.deleteObject(params, (err, data) => {
-          console.error(err);
-          console.log(data);
-          console.log(product.img.split(".com/")[1]);
-        });
+        // const params = {
+        //   Bucket: "feria-hermana",
+        //   Key: product.img.split(".com/")[1],
+        // };
+        // console.log(params);
+        // s3.deleteObject(params, (err, data) => {
+        //   console.error(err);
+        //   console.log(data);
+        //   console.log(product.img.split(".com/")[1]);
+        // });
         await product.delete()
 
       })
