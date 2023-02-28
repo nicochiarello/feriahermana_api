5ec4cdad769bf680d3e7fee1e6ffbf8eb4d9906f
Nicolas Chiarello <nicolasch.fcm@gmail.com>
2023-02-28 14:08:42 -0300

    Modified verify route

--- utils/cloudinary.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require('cloudinary')
-
-cloudinary.v2.config({
-  cloud_name: "doicfmcin",
-  api_key: "628595551931224",
-  api_secret: "sE0fTe27BqfNis5cbwVJMoXaNIg",
-});
-
-module.exports = cloudinary;
