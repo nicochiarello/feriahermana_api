0a678d5823c6513c2c2a699348a13770d9e54b35
Nicolas Chiarello <nicolasch.fcm@gmail.com>
2023-02-28 20:35:58 -0300

    Added missing logic to verify order status

--- controllers/verify.js
+++ controllers/verify.js
@@ -3,29 +3,23 @@ const Order = require("../models/orders");
 
 exports.verifyOrderStatus = async (req, res) => {
   // El id estara en req.body.data.id
-  console.log(req.body)
-    let mp_id = req.body.data.id;
-    axios
-      .get(`https://api.mercadopago.com/v1/payments/${mp_id}`, {
-        headers: {
-          Autorization: `Bearer ${process.env.MERCADO_PAGO_ACCESS_TOKEN}`,
-        },
-      })
-      .then(async (res) => {
-        if (res.status === "approved") {
-          // Cambiar estado de orden
-          // El id de la orden esta en res.metadata.orderId
-          let orderId = res.metadata.orderId;
-          await Order.findByIdAndUpdate(orderId, { status: 1 });
-          return res.status(200).json({oki: "doki"})
-        } else {
-          // otra cosa
-          let orderId = res.metadata.orderId;
-          await Order.findByIdAndUpdate(orderId, { status: 3 });
-          return res.status(200).json({oki: "doki"})
-        }
-      });
-  return res.status(200).json({ oki: "doki" });
+  let mp_id = req.body.data.id;
+  axios
+    .get(`https://api.mercadopago.com/v1/payments/${mp_id}`, {
+      headers: {
+        Authorization: `Bearer ${process.env.MERCADO_PAGO_ACCESS_TOKEN}`,
+      },
+    })
+    .then(async (response) => {
+      let orderId = response.data.metadata.order_id;
+      if (response.data.status === "approved") {
+        // Cambiar estado de orden
+        await Order.findByIdAndUpdate(orderId, { state: 1 });
+        return res.status(200).json({ oki: "doki" });
+      } else {
+        await Order.findByIdAndUpdate(orderId, { status: 2 }); 
+        return res.status(200).json({ oki: "doki" });
+      }
+    });
+
 };
-
-
